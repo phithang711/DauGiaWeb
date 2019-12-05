e434dba547138590df1b860ab8d876912d859c7f
tthanh <tranthuanthanh@users.noreply.github.com>
2019-12-06 01:21:16 +0700

    Create link for item
    Create link for item. Item view need css to be added

--- public/js/section.js
+++ public/js/section.js
@@ -1,7 +1,13 @@
-var links = document.getElementsByClassName("product-name");
-links[0].onclick = function() {
-        console.log(document.getElementsByClassName("product-name").index(this));
-    }
-    // $('product-name').click(function() {
-    //     console.log($('product-name').index(this));
-    // });
+
+// $(".product-name").attr("href",()=>{
+//     return data-type+"/"+data-index;
+// });
+
+// $(".product-name").on("click",()=>{
+//     console.log(data-type+"/"+data-index);
+// });
+
+$(".section-title").text(function(){
+    var curText=$(this).text();
+    return curText.charAt(0).toUpperCase()+curText.substring(1).toLowerCase();
+});
--- routes/index.js
+++ routes/index.js
@@ -1,212 +1,54 @@
 var express = require('express');
 var router = express.Router();
 
-let slideShowItems = [{
-        name: "N Logitech G230",
-        picture: "https://i.ebayimg.com/thumbs/images/g/ZJ0AAOSwx2dYCQBz/s-l225.webp",
-        description: "Stereo Gaming Noise-cancelling Wired Headset (981-000541)",
-        price: "22.89"
-    },
-    {
-        name: "Sony MDR-ZX110",
-        picture: "https://i.ebayimg.com/thumbs/images/m/mlnBS6Y1o8ZiNBdYlKzllhA/s-l225.jpg",
-        description: "Stereo Over-Head Headphone Extra Bass Black & White Colors",
-        price: "99"
-    },
-    {
-        name: "Shockproof Cover",
-        picture: "https://images-na.ssl-images-amazon.com/images/I/91OME-jyFmL.jpg",
-        description: "For Apple iPhone 11, 11 Pro, 11 Pro Max Case Ringke [FUSION-X]",
-        price: "7.99"
-    }
-]
-
-let window.homepageItems = [{
-        content: {
-            title: "Top",
-            items: [{
-                    name: "A4 USB LED",
-                    picture: "https://i.ebayimg.com/thumbs/images/g/qigAAOSwWAldTTSW/s-l225.webp",
-                    description: "Stereo Bass Surround Gaming Headset for PS4",
-                    price: "14.97",
-                    link: "/vatpham1"
-                },
-                {
-                    name: "ONIKUMA K1",
-                    picture: "https://images-na.ssl-images-amazon.com/images/I/91OME-jyFmL.jpg",
-                    description: "Stereo Bass Surround Gaming Headset for PS4 New Xbox One PC with Mic",
-                    price: "19.98",
-                    link: "/vatpham1"
-                },
-                {
-                    name: "3 Colors LED Backlit",
-                    picture: "https://i.ebayimg.com/thumbs/images/g/ocQAAOSwIRhd4OM5/s-l225.webp",
-                    description: "USB Wired Gaming Keyboard Multimedia and 2400 DPI Mouse Set",
-                    price: "16.14",
-                    link: "/vatpham1"
-                },
-                {
-                    name: "Logitech G203",
-                    picture: "https://i.ebayimg.com/thumbs/images/m/mZE5JW39zkUOOj1wNlAQwQg/s-l225.jpg",
-                    description: "Logitech G203 Mouse Prodigy Programmable",
-                    price: "21.95",
-                    link: "/vatpham1"
-                },
-                {
-                    name: "McAfee AntiVirus 1 PC ",
-                    picture: "https://i.ebayimg.com/thumbs/images/g/kJIAAOSwWnBctiB1/s-l225.webp",
-                    description: "Digital delivery",
-                    price: "15.99",
-                    link: "/vatpham1"
-                },
-                {
-                    name: "3 Colors LED Backlit",
-                    picture: "https://i.ebayimg.com/thumbs/images/g/ocQAAOSwIRhd4OM5/s-l225.webp",
-                    description: "USB Wired Gaming Keyboard Multimedia and 2400 DPI Mouse Set",
-                    price: "16.14",
-                    link: "/vatpham1"
-                },
-                {
-                    name: "Logitech G203",
-                    picture: "https://i.ebayimg.com/thumbs/images/m/mZE5JW39zkUOOj1wNlAQwQg/s-l225.jpg",
-                    description: "Logitech G203 Mouse Prodigy Programmable",
-                    price: "21.95",
-                    link: "/vatpham1"
-                },
-                {
-                    name: "McAfee AntiVirus 1 PC ",
-                    picture: "https://i.ebayimg.com/thumbs/images/g/kJIAAOSwWnBctiB1/s-l225.webp",
-                    description: "Digital delivery",
-                    price: "15.99",
-                    link: "/vatpham1"
-                }
-            ]
-        }
-    },
-    {
-        content: {
-            title: "Trending",
-            items: [{
-                    name: "Lords of the Rings",
-                    picture: "https://images-na.ssl-images-amazon.com/images/I/91OME-jyFmL.jpg",
-                    description: "No",
-                    price: "99",
-                    link: "/vatpham1"
-                },
-                {
-                    name: "Lords of the Rings",
-                    picture: "https://images-na.ssl-images-amazon.com/images/I/91OME-jyFmL.jpg",
-                    description: "No",
-                    price: "99",
-                    link: "/vatpham1"
-                },
-                {
-                    name: "Lords of the Rings",
-                    picture: "https://images-na.ssl-images-amazon.com/images/I/91OME-jyFmL.jpg",
-                    description: "No",
-                    price: "99",
-                    link: "/vatpham1"
-                },
-                {
-                    name: "Lords of the Rings",
-                    picture: "https://images-na.ssl-images-amazon.com/images/I/91OME-jyFmL.jpg",
-                    description: "No",
-                    price: "99",
-                    link: "/vatpham1"
-                },
-                {
-                    name: "Lords of the Rings",
-                    picture: "https://images-na.ssl-images-amazon.com/images/I/91OME-jyFmL.jpg",
-                    description: "No",
-                    price: "99",
-                    link: "/vatpham1"
-                },
-                {
-                    name: "3 Colors LED Backlit",
-                    picture: "https://i.ebayimg.com/thumbs/images/g/ocQAAOSwIRhd4OM5/s-l225.webp",
-                    description: "USB Wired Gaming Keyboard Multimedia and 2400 DPI Mouse Set",
-                    price: "16.14",
-                    link: "/vatpham1"
-                },
-                {
-                    name: "Logitech G203",
-                    picture: "https://i.ebayimg.com/thumbs/images/m/mZE5JW39zkUOOj1wNlAQwQg/s-l225.jpg",
-                    description: "Logitech G203 Mouse Prodigy Programmable",
-                    price: "21.95",
-                    link: "/vatpham1"
-                },
-                {
-                    name: "McAfee AntiVirus 1 PC ",
-                    picture: "https://i.ebayimg.com/thumbs/images/g/kJIAAOSwWnBctiB1/s-l225.webp",
-                    description: "Digital delivery",
-                    price: "15.99",
-                    link: "/vatpham1"
-                }
-            ]
-        }
-    },
-    {
-        content: {
-            title: "Ending today",
-            items: [{
-                    name: "Lords of the Rings",
-                    picture: "https://images-na.ssl-images-amazon.com/images/I/91OME-jyFmL.jpg",
-                    description: "No",
-                    price: "99"
-                },
-                {
-                    name: "Lords of the Rings",
-                    picture: "https://images-na.ssl-images-amazon.com/images/I/91OME-jyFmL.jpg",
-                    description: "No",
-                    price: "99"
-                },
-                {
-                    name: "Lords of the Rings",
-                    picture: "https://images-na.ssl-images-amazon.com/images/I/91OME-jyFmL.jpg",
-                    description: "No",
-                    price: "99"
-                },
-                {
-                    name: "Lords of the Rings",
-                    picture: "https://images-na.ssl-images-amazon.com/images/I/91OME-jyFmL.jpg",
-                    description: "No",
-                    price: "99"
-                },
-                {
-                    name: "Lords of the Rings",
-                    picture: "https://images-na.ssl-images-amazon.com/images/I/91OME-jyFmL.jpg",
-                    description: "No",
-                    price: "99"
-                }
-            ]
-        }
-    }
-]
+var products = require('./products');
 
 router.get('/', function(req, res) {
     var context = {
-        data: homepageItems,
-        slideshow: homepageItems
+        data: products.homepageItems,
+        slideshow: products.homepageItems
     };
     res.render('index', context);
 });
 
+router.get('/all', function(req, res) {
+    var context = products.all;
+    res.render('allProducts', context);
+});
+
 router.get('/login', function(req, res, next) {
-    res.render('login', { title: 'Express' });
+    res.render('login', { title: 'AuctionDealer Login' });
 });
 
 router.get('/signup', function(req, res, next) {
-    res.render('signup', { title: 'Express' });
+    res.render('signup', { title: 'AuctionDealer Sign up' });
 });
 
 router.get('/betview', function(req, res, next) {
-    res.render('betview', { title: 'Express' });
+    res.render('betview', { title: 'Betview' });
 });
 
+router.get('/uiitem', function(req, res, next) {
+    res.render('user_page/item', { title: 'Express' });
+});
 
-router.get('/vatpham1', function(req, res, next) {
-    res.render('../views/user_page/UIitem', { title: 'Express' });
-  });
+router.get('/:type/:index', function(req, res) {
+    //get param
+    var productType = req.params.type.normalize();
+    var index = parseInt(req.params.index);
+    if (productType === 'all') {
+        item = products.all.content.items[index];
+        res.render('user_page/item', item);
+
+    } else {
+        item=products.homepageItems.find(item => item.content.title === productType).content.items[index];
+        // item = products.homepageItems.filter(obj => {
+        //     return obj.content.title === productType
+        //   }).content.items[index];
+          res.render('user_page/item', item);
+    }
+    
+});
 
 
 module.exports = router;
