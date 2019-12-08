698142f9e8752527ca303632a27a2e13946ddb51
phithang711 <pthang769@gmail.com>
2019-12-08 08:03:00 +0700

    nothing

--- public/js/item.js
+++
@@ -1,3 +0,0 @@
-$('#bidnow').attr('href',window.location.pathname+'/bid');
-$('#Comment').attr('href',window.location.pathname+'/review');
-$('#merchantsite').attr('href',window.location.pathname+'/merchantview');
