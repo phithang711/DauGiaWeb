daed354ebed9f142da442abffe0182d2b95be88a
phithang711 <pthang769@gmail.com>
2019-12-08 08:02:17 +0700

    upload to master

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
 # DauGiaWeb
 Đồ án Web
-Thành's design first UI taskbar
-
-Muốn vô trang user's product thì bấm vô localhost:3000/test. Vì t chưa link trang đó vô
---
+++ routes/products.js
@@ -0,0 +1,206 @@
+var slideShowItems = [{
+        name: "N Logitech G230",
+        picture: "https://i.ebayimg.com/thumbs/images/g/ZJ0AAOSwx2dYCQBz/s-l225.webp",
+        description: "Stereo Gaming Noise-cancelling Wired Headset (981-000541)",
+        price: "22.89"
+    },
+    {
+        name: "Sony MDR-ZX110",
+        picture: "https://i.ebayimg.com/thumbs/images/m/mlnBS6Y1o8ZiNBdYlKzllhA/s-l225.jpg",
+        description: "Stereo Over-Head Headphone Extra Bass Black & White Colors",
+        price: "99"
+    },
+    {
+        name: "Shockproof Cover",
+        picture: "https://images-na.ssl-images-amazon.com/images/I/91OME-jyFmL.jpg",
+        description: "For Apple iPhone 11, 11 Pro, 11 Pro Max Case Ringke [FUSION-X]",
+        price: "7.99"
+    }
+]
+
+var homepageItems = [{
+        content: {
+            title: "top",
+            items: [{
+                    name: "A4 USB LED",
+                    picture: "https://i.ebayimg.com/thumbs/images/g/qigAAOSwWAldTTSW/s-l225.webp",
+                    description: "Stereo Bass Surround Gaming Headset for PS4",
+                    price: "14.97"
+                },
+                {
+                    name: "ONIKUMA K1",
+                    picture: "https://images-na.ssl-images-amazon.com/images/I/91OME-jyFmL.jpg",
+                    description: "Stereo Bass Surround Gaming Headset for PS4 New Xbox One PC with Mic",
+                    price: "19.98"
+                },
+                {
+                    name: "3 Colors LED Backlit",
+                    picture: "https://i.ebayimg.com/thumbs/images/g/ocQAAOSwIRhd4OM5/s-l225.webp",
+                    description: "USB Wired Gaming Keyboard Multimedia and 2400 DPI Mouse Set",
+                    price: "16.14"
+                },
+                {
+                    name: "Logitech G203",
+                    picture: "https://i.ebayimg.com/thumbs/images/m/mZE5JW39zkUOOj1wNlAQwQg/s-l225.jpg",
+                    description: "Logitech G203 Mouse Prodigy Programmable",
+                    price: "21.95"
+                },
+                {
+                    name: "McAfee AntiVirus 1 PC ",
+                    picture: "https://i.ebayimg.com/thumbs/images/g/kJIAAOSwWnBctiB1/s-l225.webp",
+                    description: "Digital delivery",
+                    price: "15.99"
+                },
+                {
+                    name: "3 Colors LED Backlit",
+                    picture: "https://i.ebayimg.com/thumbs/images/g/ocQAAOSwIRhd4OM5/s-l225.webp",
+                    description: "USB Wired Gaming Keyboard Multimedia and 2400 DPI Mouse Set",
+                    price: "16.14"
+                },
+                {
+                    name: "Logitech G203",
+                    picture: "https://i.ebayimg.com/thumbs/images/m/mZE5JW39zkUOOj1wNlAQwQg/s-l225.jpg",
+                    description: "Logitech G203 Mouse Prodigy Programmable",
+                    price: "21.95"
+                },
+                {
+                    name: "McAfee AntiVirus 1 PC ",
+                    picture: "https://i.ebayimg.com/thumbs/images/g/kJIAAOSwWnBctiB1/s-l225.webp",
+                    description: "Digital delivery",
+                    price: "15.99"
+                }
+            ]
+        }
+    },
+    {
+        content: {
+            title: "trending",
+            items: [{
+                    name: "Lords of the Rings",
+                    picture: "https://images-na.ssl-images-amazon.com/images/I/91OME-jyFmL.jpg",
+                    description: "No",
+                    price: "99"
+                },
+                {
+                    name: "Lords of the Rings",
+                    picture: "https://images-na.ssl-images-amazon.com/images/I/91OME-jyFmL.jpg",
+                    description: "No",
+                    price: "99"
+                },
+                {
+                    name: "Lords of the Rings",
+                    picture: "https://images-na.ssl-images-amazon.com/images/I/91OME-jyFmL.jpg",
+                    description: "No",
+                    price: "99"
+                },
+                {
+                    name: "Lords of the Rings",
+                    picture: "https://images-na.ssl-images-amazon.com/images/I/91OME-jyFmL.jpg",
+                    description: "No",
+                    price: "99"
+                },
+                {
+                    name: "Lords of the Rings",
+                    picture: "https://images-na.ssl-images-amazon.com/images/I/91OME-jyFmL.jpg",
+                    description: "No",
+                    price: "99"
+                },
+                {
+                    name: "3 Colors LED Backlit",
+                    picture: "https://i.ebayimg.com/thumbs/images/g/ocQAAOSwIRhd4OM5/s-l225.webp",
+                    description: "USB Wired Gaming Keyboard Multimedia and 2400 DPI Mouse Set",
+                    price: "16.14"
+                },
+                {
+                    name: "Logitech G203",
+                    picture: "https://i.ebayimg.com/thumbs/images/m/mZE5JW39zkUOOj1wNlAQwQg/s-l225.jpg",
+                    description: "Logitech G203 Mouse Prodigy Programmable",
+                    price: "21.95"
+                },
+                {
+                    name: "McAfee AntiVirus 1 PC ",
+                    picture: "https://i.ebayimg.com/thumbs/images/g/kJIAAOSwWnBctiB1/s-l225.webp",
+                    description: "Digital delivery",
+                    price: "15.99"
+                }
+            ]
+        }
+    },
+    {
+        content: {
+            title: "ending",
+            items: [{
+                    name: "Lords of the Rings",
+                    picture: "https://images-na.ssl-images-amazon.com/images/I/91OME-jyFmL.jpg",
+                    description: "No",
+                    price: "99"
+                },
+                {
+                    name: "Lords of the Rings",
+                    picture: "https://images-na.ssl-images-amazon.com/images/I/91OME-jyFmL.jpg",
+                    description: "No",
+                    price: "99"
+                },
+                {
+                    name: "Lords of the Rings",
+                    picture: "https://images-na.ssl-images-amazon.com/images/I/91OME-jyFmL.jpg",
+                    description: "No",
+                    price: "99"
+                },
+                {
+                    name: "Lords of the Rings",
+                    picture: "https://images-na.ssl-images-amazon.com/images/I/91OME-jyFmL.jpg",
+                    description: "No",
+                    price: "99"
+                },
+                {
+                    name: "Lords of the Rings",
+                    picture: "https://images-na.ssl-images-amazon.com/images/I/91OME-jyFmL.jpg",
+                    description: "No",
+                    price: "99"
+                }
+            ]
+        }
+    }
+]
+
+var all = {
+    content: {
+        title: "all",
+        items: [{
+                name: "Lords of the Rings",
+                picture: "https://images-na.ssl-images-amazon.com/images/I/91OME-jyFmL.jpg",
+                description: "No",
+                price: "99"
+            },
+            {
+                name: "Lords of the Rings",
+                picture: "https://images-na.ssl-images-amazon.com/images/I/91OME-jyFmL.jpg",
+                description: "No",
+                price: "99"
+            },
+            {
+                name: "Lords of the Rings",
+                picture: "https://images-na.ssl-images-amazon.com/images/I/91OME-jyFmL.jpg",
+                description: "No",
+                price: "99"
+            },
+            {
+                name: "Lords of the Rings",
+                picture: "https://images-na.ssl-images-amazon.com/images/I/91OME-jyFmL.jpg",
+                description: "No",
+                price: "99"
+            },
+            {
+                name: "Lords of the Rings",
+                picture: "https://images-na.ssl-images-amazon.com/images/I/91OME-jyFmL.jpg",
+                description: "No",
+                price: "99"
+            }
+        ]
+    }
+}
+
+module.exports.homepageItems = homepageItems;
+module.exports.slideShowItems = slideShowItems;
+module.exports.all = all;
