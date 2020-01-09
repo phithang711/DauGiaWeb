6bf96c701bc2d36f99a5feb03117b050775664d4
tthanh <tranthuanthanh@users.noreply.github.com>
2020-01-09 17:42:19 +0700

    Edit category

--- models/category.model.js
+++ models/category.model.js
@@ -2,7 +2,9 @@ const db = require('../utils/database.util');
 
 module.exports = {
     all: _ => db.load('select * from category'),
-    add: _ => db.load('select * from category'),
+    add: (cate) => db.load(`INSERT INTO category(category_name) VALUES ('${cate}')`),
+    edit: (cate, oldcate) => db.load(`UPDATE category SET category_name = '${cate}' WHERE category_name = '${oldcate}'`),
     clear: _ => db.load('select * from category'),
     addList: _ => db.load('select * from category'),
+    remove: (cate) => db.load(`DELETE FROM category WHERE (category_name)='${cate}'`),
 };
--- routes/admin/manage.route.js
+++ routes/admin/manage.route.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const userModel = require('../../models/user.model');
 const tomerchantModel = require('../../models/tomerchant');
+const categoryModel = require("../../models/category.model");
 
 router.get('/manage', async function(req, res, next) {
     const accountList = await userModel.all();
@@ -72,10 +73,54 @@ router.post('/manage/downgrade', async function(req, res, next) {
     res.render('admin/manage', { items: accountList });
 });
 
-router.get('/manage/category', function(req, res, next) {
-    //let accountList = await
-    //res.render('admin/manage', accountList);
+router.get('/manage/category', async function(req, res, next) {
+    let categories = await categoryModel.all();
+    console.log(categories);
+    res.render('admin/manageCategories', { cats: categories });
 });
+
+router.post('/manage/category/delete', async function(req, res, next) {
+    var cate = req.body.category;
+
+    var accountList = await categoryModel.remove(cate);
+
+    if (accountList) {
+        res.send("category deleted");
+    } else {
+        res.status(400).send("Can't delete");
+    }
+});
+
+router.post('/manage/category/edit', async function(req, res, next) {
+    var cate = req.body.cate;
+    var oldcate = req.body.oldcate;
+
+    var accountList = await categoryModel.edit(cate, oldcate);
+
+    if (accountList) {
+        res.send("category edited");
+    } else {
+        res.status(400).send("Can't delete");
+    }
+    res.send(req.body.cate);
+});
+
+router.post('/manage/category/add', async function(req, res, next) {
+    var cate = req.body.cate;
+
+    console.log(cate);
+    if (cate !== "") {
+        var accountList = await categoryModel.add(cate);
+    }
+
+    if (accountList) {
+        res.send("category added");
+    } else {
+        res.status(400).send("Can't delete");
+    }
+});
+
+
 
 
 module.exports = router;
