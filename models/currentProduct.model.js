4adb4f991a3a155d2b291777b5db3ecf389a9dc1
phithang711 <pthang769@gmail.com>
2020-01-10 04:16:35 +0700

    Change skip OTP on sign up page

--- models/currentProduct.model.js
+++ models/currentProduct.model.js
@@ -3,8 +3,5 @@
  module.exports = {
      all: _ => db.load('select * from current_product'),
      getById: (id) => db.load("SELECT * FROM current_product WHERE (id)=" + id),
-     add: (id) => {
-        var query = "INSERT INTO current_product(product_id) VALUES ("+id+")"
-        return db.load(query);
-     }
+     add: (id) => db.load(`INSERT INTO current_product(product_id) VALUES ('${id}')`),
  };
